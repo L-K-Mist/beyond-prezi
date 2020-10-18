882d6f3b3cfbf2d944958f1ec74a7e2c79cc1933
L-K-Mist <vandenbosch.dylan1@gmail.com>
2020-10-18 09:41:27 +0200

    Added stepper infographic.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
+body {
+  margin: 0;
+  overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
